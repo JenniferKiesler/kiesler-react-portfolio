3912534e53ccbf532c9804b8b909af57a5703eb5
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2023-02-08 23:25:59 -0600

    edits footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ const Footer = props => {
   return (
     <footer className="mt-5">
       <div className="d-flex justify-content-center mt-3 pt-3 pb-1">
-        <a href="https://github.com/JenniferKiesler" target="_blank">
+        <a href="https://github.com/JenniferKiesler" target="_blank" rel="noreferrer">
           <i className="bi bi-github m-3 fs-2"></i>
         </a>
-        <a href="https://www.linkedin.com/in/jennifer-kiesler-aa469591/" target="_blank">
+        <a href="https://www.linkedin.com/in/jennifer-kiesler-aa469591/" target="_blank" rel="noreferrer">
           <i className="bi bi-linkedin m-3 fs-2"></i>
         </a>
-        <a href="https://stackoverflow.com/users/21101475/jennifer-kiesler" target="_blank">
+        <a href="https://stackoverflow.com/users/21101475/jennifer-kiesler" target="_blank" rel="noreferrer">
           <i className="bi bi-stack-overflow m-3 fs-2"></i>
         </a>
         <a href="mailto:kiesler.jen@gmail.com">
